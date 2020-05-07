b3a1f1b7fdcf25b4094dfa3ae531aa447102b9a7
Kyle Dahl <kyle.dahl201@gmail.com>
2020-05-06 22:10:25 -0500

    api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,48 +3,32 @@ const workOuts = require("../models/excercises.js");
 
 router.put("/api/workouts/:id", function(req,res){
 //update workout
-
+workOuts.findByIdAndUpdate(
+    params.id,{
+        $push: {exercises: body} },
+        {new: true}
+).then (data => res.json(data))
+.catch(err => {
+    console.log(err)
+    res.json(err)
 })
 
 router.post("/api/workouts/", function(req,res){
 //create workout
+workOuts.create({}).then(data =>
+    res.json(data)).catch(err => {
+        console.log(err)
+        res.json(err)
 })
 
 router.get("/api/workouts/range", function(req,res){
 //query database for workouts
+workOuts.find().then(data => {
+    res.json(data)
+}).catch(err => {
+    res.json(err)
 
 })
-
-// router.post("/api/transaction", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
 
 
 
